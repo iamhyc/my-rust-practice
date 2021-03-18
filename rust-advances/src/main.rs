f9e8db0dbd592a0cd0f3849061a497bdd1c03e48
iamhyc <sudofree@163.com>
2021-03-18 21:47:56 +0800

    code for today...

--- rust-advances/src/main.rs
+++ rust-advances/src/main.rs
@@ -1,8 +1,37 @@
 
-fn test_ownership() {
+use std::fmt;
 
+struct Integer(i32);
+// impl for struct Integer (with std::fmt interface)
+impl fmt::Display for Integer {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
 }
 
+fn test_ownership(str: String) -> String {
+    let mut new_str = str.clone();
+    new_str.push_str("_real");
+    println!("{} is taken from main.", str);
+    return new_str; //ownership of "&mut String" moved out to "&String"
+    //(non-sense but interesting...)
+}
+
+/** doc line? okay, this is the main function. */
 fn main() {
-    println!("Hello, world!");
+    // definition of scope
+    {
+        let s:&str = "test";
+        println!("test scope with: \"{}\"",s);
+    }
+
+    // &str and String
+    let mut s = String::from("s: &str");
+    s.push_str("string: &str");
+    let ss = s; //the owner is unique on heap (so "s" is invalid now)
+    let ss = test_ownership(ss);
+    println!("test move: {}", ss);
+    
+    // reference or borrowing
+    
 }
