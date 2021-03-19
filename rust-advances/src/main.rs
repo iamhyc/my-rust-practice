f3ed2fed03bb72a9dd8cba48003262d037b36397
iamhyc <sudofree@163.com>
2021-03-19 19:33:54 +0800

    add struct && enum practice

--- rust-advances/src/main.rs
+++ rust-advances/src/main.rs
@@ -22,6 +22,22 @@ fn test_mut_literal(str: &str) {
     println!("nothing serious happened. {}", str);
 }
 
+#[derive(Debug)]
+struct Test {
+    int: i32,
+    str: String,
+}
+
+impl Test {
+    fn test(&self) {
+        println!("This is a Test.test function");
+    }
+    fn associated() {
+        ("This is a Test::associated function");
+    }
+}
+
+
 /** doc line? okay, this is the main function. */
 fn main() {
     // definition of scope
@@ -37,13 +53,37 @@ fn main() {
     let ss = test_ownership(ss);
     println!("test move: {}", ss);
     
-    // reference or borrowing
+    // reference and borrowing
     /*
      * when using borrowing (copy/drop trait), the assigned variable keeps "mut" attribute;
      * when using reference, reference itself (i.e., the pointer) is borrowed;
      * Moreover, "&var" is always okay, "&mut var" only okay with one-time borrowing (::sad)
     */
-    let s = "literal_string";
-    test_mut_literal(&s);
+    let s = "literal_string"; // non-sense using mut
+    test_mut_literal(&s);     // non-sense using mut
+    let tmp = &s[ .. s.find("_").unwrap() ];
+    println!("slice reference by split: {}", tmp);
+    let tmp:[i32; 3] = [1,2,3];
+    println!("display for slice 0-th element: {}", &tmp[..][0]); 
+
+    // struct and impl
+    let test = Test{int:1, str:String::from("A")};
+    let mut test1 = Test {..test}; // (bad practice) this is a borrow action
+    test1.str.push_str("string: &str");
+    println!("display that struct instance move (int {} remains): {:?}", test.int, test1);
+    test1.test(); //`test.test()` not working for value "partially move"
+    Test::associated();
+
+    // enums and pattern matching
+    let x = Option::Some("Some");
+    match x {
+        Option::Some(val) => println!("match some: {}", val),
+        // Option::None => println!("None!"),
+        _ => () //funny, very different from Python
+    }
+    if let Some(val) = x { // (bad practice) syntax sugar
+        println!("(if let) match some: {}", val);
+        println!("(if let) {:?}, x is not borrowed.", x);
+    }
 
 }
