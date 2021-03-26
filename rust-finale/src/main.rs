429aa39f1865b1b5158dcda658f6235345c1d7ac
iamhyc <sudofree@163.com>
2021-03-26 22:23:21 +0800

    code for today

--- rust-finale/src/main.rs
+++ rust-finale/src/main.rs
@@ -1,7 +1,18 @@
 
+
+fn use_closure<T>(func:T)->i32
+    where T: Fn(i32) -> i32
+{
+    func(10)
+}
+
 fn main() {
+    let _tmp = 32;
+
+    //the type of "a:i32" and return is inferred from usage
     let _closure = |a| {
-        a
-    }
-    println!("Hello, world!");
+        a + _tmp
+    };
+
+    println!( "Hello, world! {}", use_closure(_closure) );
 }
