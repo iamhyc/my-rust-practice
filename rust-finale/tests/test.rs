6f981ef8f404d305411e3ed378adea932f73f379
iamhyc <sudofree@163.com>
2021-03-30 21:01:55 +0800

    update thread test

--- rust-finale/tests/test.rs
+++ rust-finale/tests/test.rs
@@ -1,10 +1,17 @@
 
+use std::thread;
+use std::time::Duration;
 
-#[cfg(test)]
 mod concurrency_test {
+    use super::*;
 
     #[test]
     fn test() {
-        
+        let _handler = thread::spawn(|| {
+            thread::sleep(Duration::from_millis(500));
+            println!("delayed response from spawned.");
+        });
+
+        println!("response from main thread.");
     }
 }
