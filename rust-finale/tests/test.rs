056c214e1d3c1ce9af38d0032902ba93056f71c3
iamhyc <sudofree@163.com>
2021-04-02 15:49:52 +0800

    update channel comm test

--- rust-finale/tests/test.rs
+++ rust-finale/tests/test.rs
@@ -1,12 +1,13 @@
 
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc;
 
 mod concurrency_test {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_thread() {
         let _handler = thread::spawn(|| {
             thread::sleep(Duration::from_millis(500));
             println!("delayed response from spawned.");
@@ -14,5 +15,20 @@ mod concurrency_test {
 
         println!("response from main thread.");
         _handler.join().unwrap();
+    }
+
+    #[test]
+    fn test_channel() {
+        let (_tx, rx) = mpsc::channel::<i32>();
+        
+        let tx = _tx.clone();
+        thread::spawn(move || {
+            for i in 100..123 {
+                tx.send(i).unwrap();
+                thread::sleep(Duration::from_millis(10)); //send every 10ms
+            }
+        });
+
+        println!("blocking received: {}", rx.recv().unwrap());
     }
 }
